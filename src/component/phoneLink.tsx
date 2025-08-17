4d4947a3183b2de89ede8d9e067dff30f28e280b
Brian-drift <nonobrian4gmail.com>
2025-08-17 18:02:29 +0200

    Ajouts d'un lien les appelles normales.

--- src/component/phoneLink.tsx
+++ src/component/phoneLink.tsx
@@ -1,8 +1,10 @@
 import phone from '../assets/img_3.png'
 
 function PhoneLink() {
+    const phoneNumber = "243843193064";
+    const phoneLink = `tel:${phoneNumber}`;
     return (
-        <a href={"+243843193064"} target="_blank" rel="noopener noreferrer">
+        <a href={phoneLink} target="_blank" rel="noopener noreferrer">
             <img src={phone} alt ="Contacter" style={{ width: '50px', height: '50px', borderRadius: '50%', paddingLeft : "15px",}} />
         </a>
     );
